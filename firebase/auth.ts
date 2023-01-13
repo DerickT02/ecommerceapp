676d8700ea03890d4f543f2631997e3ec45382d8
DerickT02 <derickt18@outlook.com>
2023-01-13 14:46:58 -0800

    Push a working version in case something goes wrong

--- firebase/auth.ts
+++ firebase/auth.ts
@@ -1,6 +1,24 @@
-import { auth } from "./config";
+import { auth, db } from "./config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore"
 
 
-export async function signUp(){}
-export async function signIn(){}
-export async function logout(){}
+export async function signUp(email: string, password: string){
+    let credential = await createUserWithEmailAndPassword(auth, email, password)
+    console.log(credential)
+    await setDoc(doc(db, "users", credential.user.uid), {
+        email: email,
+        role: "customer",
+        cart: []
+    }).then(() => {
+        return "successfully created customer"
+    }).catch(err => {
+        console.log(err)
+    })
+}
+export function signIn(){
+    return false
+}
+export function logout(){
+    return false
+}
