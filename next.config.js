60b642b658542248209e6307639e5c3e60702c54
DerickT02 <derickt18@outlook.com>
2023-01-14 11:18:15 -0800

    Added Unresponsive Nav UI to homepage

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images:{
+    remotePatterns:[
+      {
+        protocol: "https",
+        hostname:"firebasestorage.googleapis.com",
+        pathname:"/v0/b/ecommerceproject-416fd.appspot.com/o/"
+
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
